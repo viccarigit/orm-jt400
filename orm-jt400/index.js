429757936eb480df3d50cb54476b6fbbc9d0126c
Eduardo Viccari <viccari.dev@gmail.com>
2019-10-11 15:22:43 -0300

    new adjusts

--- orm-jt400/index.js
+++ orm-jt400/index.js
@@ -7,10 +7,13 @@ function logger(message){
 function mappedSchema(){
 
     const loja = {
-        __entity: 'store',
-        __schema: 'development',
-        id: {fieldName: 'store_id', isId: true},
-        description: {fieldName: 'store_description'}
+        __entity: 'co000t',
+        __schema: 'prod',
+        id: {fieldName: 'cd_fl', isId: true},
+        description: {fieldName: 'no_fl'},
+        alias: {fieldName: 'mn_fl'},
+        type: {fieldName: 'tp_fl'},
+        shippAddress: {fieldName: 'ds_end'}
     };
 
     return loja;
@@ -23,9 +26,26 @@ async function findById(mappedSchema){
 logger('running tests...');
 
 var store = mappedSchema();
+
+store.status = crudSupport.innerJoin({
+    __entity: 'co001t',
+    __referencedBy : {origem: 'co000t', fromColumn: 'cd_sts', toColumn: 'cd_sts'},
+    id: {fieldName: 'cd'}
+    }
+);
+
+store.status = crudSupport.leftJoin({
+    __entity: 'co010t',
+    __referencedBy : [
+            {target: 'co000t', fromColumn: 'cd_user', toColumn: 'cd_user'},
+            {target: 'co000t', fromColumn: 'cd_user', toColumn: 'cd_user'},
+        ]
+});
+
+
 store.id.value = 100;
 
 findById(store).then(data =>{
-    console.log(data);
+    console.log(JSON.stringify(data));
 });
 
